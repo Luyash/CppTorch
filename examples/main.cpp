6080430acd57edb754fa19ae339b099da14aee7a
yepMizu <luyashthapa123@gmail.com>
2026-09-03 22:12:32 +0545

    I have no idea what this mean

---
+++ examples/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <Eigen/Dense>
+
+int main()
+{
+    Eigen::MatrixXd A(2,2);
+
+    A << 1, 2,
+         3, 4;
+
+    Eigen::MatrixXd B(2,2);
+
+    B << 5, 6,
+         7, 8;
+
+    std::cout << "Matrix A\n" << A << "\n\n";
+    std::cout << "Matrix B\n" << B << "\n\n";
+
+    std::cout << "A + B\n" << A + B << "\n\n";
+    std::cout << "A * B\n" << A * B << "\n";
+
+    return 0;
+}
