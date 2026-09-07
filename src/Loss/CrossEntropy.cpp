f26aafc506b0bdf02a876bc205609d9602862bbd
yepMizu <luyashthapa123@gmail.com>
2026-09-07 23:20:23 +0545

    Implementing Cross Entropy Loss function

---
+++ src/Loss/CrossEntropy.cpp
@@ -0,0 +1,12 @@
+#include <CppTorch/Loss/CrossEntropy.h>
+
+namespace CppTorch
+{
+    double CrossEntropy(
+        const Eigen::MatrixXd& predicted,
+        const Eigen::MatrixXd& actual
+    )
+    {
+        // Your implementation
+    }
+}
