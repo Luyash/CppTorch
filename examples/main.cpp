7233ee064f189e9a108714b3f97f29be3c45de32
yepMizu <luyashthapa123@gmail.com>
2026-09-07 23:23:23 +0545

    Paths

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
 #include "../include/CppTorch/Layers/Dense.h"
+#include "../include/CppTorch/Loss/MSE.h"
+#include "../include/CppTorch/Loss/BinaryCrossEntropy.h"
+
 
 int main()
 {
