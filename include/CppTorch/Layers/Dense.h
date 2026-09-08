c35655cf6392a30919f7bbb14d1db34ca8749fc9
yepMizu <luyashthapa123@gmail.com>
2026-09-08 14:57:28 +0545

    Minor change in Dense.h

--- include/CppTorch/Layers/Dense.h
+++ include/CppTorch/Layers/Dense.h
@@ -1,7 +1,10 @@
 #pragma once
 
 #include <Eigen/Dense>
+#include <memory>
+
 #include "Layers.h"
+#include "../Activation/Activation.h"
 
 namespace CppTorch
 {
@@ -11,8 +14,14 @@ namespace CppTorch
         Eigen::MatrixXd weights;
         Eigen::MatrixXd bias;
 
+        std::unique_ptr<Activation> activation;
+
     public:
-        Dense(int input_size, int output_size);
+        Dense(
+            int input_size,
+            int output_size,
+            std::unique_ptr<Activation> activation
+        );
 
         Eigen::MatrixXd forward(
             const Eigen::MatrixXd& input
